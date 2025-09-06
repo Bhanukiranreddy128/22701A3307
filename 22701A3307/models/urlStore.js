f07a3a80359bbd9ab9c9a7d54e4ad1798f3ab781
Bhanukiran Reddy Banala <bhanukiranreddy128@gmail.com>
2025-09-06 20:34:30 +0530

    Delete 22701A3307 directory

--- 22701A3307/models/urlStore.js
+++
@@ -1,4 +0,0 @@
-// models/urlStore.js
-// In-memory store (process-level uniqueness)
-const urlStore = new Map(); // shortcode -> { originalUrl, createdAt, expiry, clicks[] }
-module.exports = urlStore;
--- 22701A3307/routes/shorturls.js
+++
@@ -1,9 +0,0 @@
-// routes/shorturls.js
-const express = require("express");
-const router = express.Router();
-const controller = require("../controllers/shorturlsController");
-
-router.post("/", controller.createShortUrl);
-router.get("/:shortcode", controller.getStats);
-
-module.exports = router;
